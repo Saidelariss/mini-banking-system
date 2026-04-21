72594f3784d313f8e91696344cece4f149e62c6c
Said EL ARISS EL IDRISSI <saidariss3@gmail.com>
2026-04-21 20:26:44 +0100

    implement transfer service

--- src/main/java/com/services/banking/dtos/request/TransferRequest.java
+++ src/main/java/com/services/banking/dtos/request/TransferRequest.java
@@ -1,13 +1,17 @@
 package com.services.banking.dtos.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
 public class TransferRequest {
+    @NotNull(message = "sourceAccountId is required")
     private Integer sourceAccountId;
+    @NotNull(message = "destinationAccountId is required")
     private Integer destinationAccountId;
+    @NotNull(message = "amount is required")
     private BigDecimal amount;
     private String description;
 }
--- src/main/java/com/services/banking/services/TransferServiceImpl.java
+++ src/main/java/com/services/banking/services/TransferServiceImpl.java
@@ -2,28 +2,76 @@ package com.services.banking.services;
 
 import com.services.banking.dtos.request.AmountRequest;
 import com.services.banking.dtos.request.TransferRequest;
-import com.services.banking.dtos.response.TransactionResponse;
 import com.services.banking.dtos.response.TransferResponse;
+import com.services.banking.enums.TransactionStatus;
+import com.services.banking.enums.TransactionType;
+import com.services.banking.persistence.entities.AccountEntity;
+import com.services.banking.persistence.entities.TransactionEntity;
+import com.services.banking.persistence.repositories.AccountJpaRepository;
+import com.services.banking.persistence.repositories.TransactionJpaRepository;
+import com.services.banking.services.exceptions.FunctionalError;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @AllArgsConstructor
-public class TransferServiceImpl implements TransferService{
+public class TransferServiceImpl implements TransferService {
     private AccountService accountService;
+    private AccountJpaRepository accountJpaRepository;
+    private TransactionJpaRepository transactionJpaRepository;
+
     @Override
     public TransferResponse doTransfer(TransferRequest request) {
-        AmountRequest amountRequest = AmountRequest.builder()
+        AmountRequest amountRequest = buildAmountRequest(request);
+
+        AccountEntity sourceAccount = getAccountEntity(request.getSourceAccountId());
+        AccountEntity destinationAccount = getAccountEntity(request.getDestinationAccountId());
+        accountService.deposit(request.getDestinationAccountId(), amountRequest);
+        accountService.withdraw(request.getSourceAccountId(), amountRequest);
+
+        TransactionEntity savedTransactionEntity = saveTransaction(amountRequest, sourceAccount, destinationAccount);
+        return buildTransferResponse(savedTransactionEntity);
+
+
+    }
+
+    private static AmountRequest buildAmountRequest(TransferRequest request) {
+        return AmountRequest.builder()
                 .amount(request.getAmount())
                 .description(request.getDescription())
                 .build();
-
-        TransactionResponse depositResponse = accountService.deposit(request.getDestinationAccountId(), amountRequest);
-        TransactionResponse withdrawResponse = accountService.withdraw(request.getSourceAccountId(), amountRequest);
-
-        return TransferResponse.builder()
-                .amount(depositResponse.getAmount())
-                .description(depositResponse.getDescription())
-                .destinationAccountId()
     }
+
+    private static TransferResponse buildTransferResponse(TransactionEntity savedTransactionEntity) {
+        return TransferResponse.builder()
+                .id(savedTransactionEntity.getId())
+                .amount(savedTransactionEntity.getAmount())
+                .description(savedTransactionEntity.getDescription())
+                .destinationAccountId(savedTransactionEntity.getDestinationAccount().getId())
+                .sourceAccountId(savedTransactionEntity.getSourceAccount().getId())
+                .createdAt(savedTransactionEntity.getCreatedAt())
+                .status(savedTransactionEntity.getStatus())
+                .reference(savedTransactionEntity.getReference())
+                .build();
+    }
+
+    private AccountEntity getAccountEntity(Integer accountId) {
+        return accountJpaRepository.findById(accountId)
+                .orElseThrow(() -> new FunctionalError("account with id " + accountId + " not found"));
+    }
+
+    private TransactionEntity saveTransaction(AmountRequest request, AccountEntity sourceAccount, AccountEntity destinationAccount) {
+        TransactionEntity transactionEntity = new TransactionEntity();
+        transactionEntity.setReference(UUID.randomUUID().toString());
+        transactionEntity.setAmount(request.getAmount());
+        transactionEntity.setDescription(request.getDescription());
+        transactionEntity.setDestinationAccount(destinationAccount);
+        transactionEntity.setSourceAccount(sourceAccount);
+        transactionEntity.setStatus(TransactionStatus.SUCCESS);
+        transactionEntity.setType(TransactionType.TRANSFER);
+        return transactionJpaRepository.save(transactionEntity);
+    }
+
 }

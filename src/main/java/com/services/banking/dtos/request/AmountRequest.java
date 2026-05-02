5cd271637010cb049626969f0a29f71707a157fe
Said EL ARISS EL IDRISSI <saidariss3@gmail.com>
2026-05-02 19:00:35 +0100

    add lombok annotation to AmountRequest class

--- src/main/java/com/services/banking/dtos/request/AmountRequest.java
+++ src/main/java/com/services/banking/dtos/request/AmountRequest.java
@@ -1,13 +1,17 @@
 package com.services.banking.dtos.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AmountRequest {
     @NotNull(message = "Amount is required")
     private BigDecimal amount;
